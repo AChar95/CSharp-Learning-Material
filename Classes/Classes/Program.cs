3a0dde5d0a0cfc60a68435e7ae3246f7979b05ee
AChar95 <allancharles95@outlook.com>
2020-03-28 19:53:22 +0000

    added Arrays and updated Classes

--- Classes/Classes/Program.cs
+++ Classes/Classes/Program.cs
@@ -18,4 +18,17 @@ namespace Classes
 
         }
     }
+    //Please note that there is alternative to classes, these are structs.44
+    //Struct are subtly different from classes, in most use cases classes are enough, structs are used for small lightweight objects that typically are not manipulated again, see below for an example
+
+    public struct RgbColour
+    {
+        public int Red;
+        public int Green;
+        public int Blue;
+    }
+
+    // This could have been done with a class but if thousands of these are to be created then it is more efficient/effective to use a struct.
+    // One of the main differences between a struct and class is that classes can inherit from other classes whereas structs cannot, not even from other structs.
+    //They cannot be abstract or virtual unlike classes. They must be initialised before being used
 }
