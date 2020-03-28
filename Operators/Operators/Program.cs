c71ebdc79cc82301a2444ef769e0c7e8b740bcdd
AChar95 <allancharles95@outlook.com>
2020-03-28 18:55:06 +0000

    added README, Operators

---
+++ Operators/Operators/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 10;
+            var b = 3;
+            var c = 2;
+            Console.WriteLine(a + b); // 13
+            Console.WriteLine(a / b); // 3, as both 'a' and 'b' are both of type int, therefore the numbers after the decimal point do not exist (for this type)
+            Console.WriteLine((float)a / (float)b); //This will return 3.3333333 as the value returned is a floating point number
+            
+            //In the following lines the program behaves similar to standard mathematics where C# will follow BOMDAS (Brackets Order Multiplication Division Addition and Subtraction)
+            Console.WriteLine(a + b * c); // 16
+            Console.WriteLine((a + b) * c); // 26
+
+            //Comparison operators
+            Console.WriteLine( a < b ); //false
+            Console.WriteLine( a == b ); //false 
+            Console.WriteLine( a != b ); //true      !(a != b) would give false, but this bad practice and difficult to follow
+            Console.WriteLine( a > b && a > c); // true
+            Console.WriteLine( a > b && b == c); // false -> as true AND false = false
+            Console.WriteLine( a > b || b == c); // true -> as true OR false = true
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# "C#" Learning Repo
+
+This is a repository for the step-by-step learning for understanding C#, each program written in C# is designed to demonstrate the different topics covered by their titles.
+
+It should be looked at in the following order:
+1. Hello World
+2. Variables
+3. Type Conversion
+4. Operators
